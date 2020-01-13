ed835abdfd982d63a8abdd8c7c68d1350ec47242
Abdul Qayyum <khoi00091@gmail.com>
2020-01-13 16:47:23 +0800

    Referencing Multiple Dagger Dependencies within a Single Module

--- app/src/main/java/com/khoi/daggarpractice/AuthActivity.java
+++ app/src/main/java/com/khoi/daggarpractice/AuthActivity.java
@@ -2,8 +2,12 @@ package com.khoi.daggarpractice;
 
 
 import android.app.Activity;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.ImageView;
+
+import com.bumptech.glide.RequestManager;
 
 import javax.inject.Inject;
 
@@ -17,18 +21,23 @@ public class AuthActivity extends DaggerAppCompatActivity{
     private static final String TAG = "AuthActivity";
 
     @Inject
-    String someString;
-
+    Drawable logo;
     @Inject
-    boolean isAppNull;
+    RequestManager requestManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_auth);
 
-        Log.d(TAG, "onCreate: " + someString);
-        Log.d(TAG, "onCreate: is app null? " + isAppNull);
+        setLogo();
+    }
+
+
+    private void setLogo(){
+        requestManager
+                .load(logo)
+                .into((ImageView)findViewById(R.id.login_logo));
     }
 
 }
